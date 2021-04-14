69ec5af2e7750c11687384a15436af96437aab7c
nanbase <nanecadigal@gmail.com>
2021-04-14 08:44:53 +0800

    Initial commit

---
+++ src/Container.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Container extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>This is the container</h1>
+                <p>All information will be put here.</p>
+            </div>
+        )
+    }
+}
+
+export default Container
